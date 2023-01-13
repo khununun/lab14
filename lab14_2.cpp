041a157af89771939a78bd6aaa4a8286b0268312
khununun <khununun2546@gmail.com>
2023-01-13 17:15:58 +0700

    Done

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,46 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void showImage(const bool image[N][M])
+{
+    for(int a = 0; a < 72; a++)
+    {
+        cout << '-';
+    }
+    cout << '\n';
+    for(int b = 0; b < 30; b++)
+    {
+        cout << '|';
+        for(int c = 0; c < 70; c++)
+        {
+            if(image[b][c] == 0)
+            {
+                cout << ' ';
+            }
+            else if(image[b][c] == 1)
+            {
+                cout << '*';
+            }
+        }
+        cout << '|';
+        cout << '\n';
+    }
+    for(int d = 0; d < 72; d++)
+    {
+        cout << '-';
+    }
+    cout << '\n';
+}
+void updateImage(bool image[N][M],int x,int y,int z)
+{
+    for(int A = 0; A < 30; A++)
+    {
+        for(int B = 0; B < 70; B++)
+        {
+            if(sqrt(pow((A-y),2)+pow((B-z),2)) <= x-1)
+            {
+                image[A][B] = 1 ;
+            }
+        }
+    }
+}
